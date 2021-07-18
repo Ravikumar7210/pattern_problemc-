ccdb447be4ea376cd4c54bada676b1d1b9d4fe9c
ravi kumar <rk76912@gmail.com>
2021-07-18 15:31:21 +0530

    practice

--- hollow_rectangle_pattern.cpp
+++ hollow_rectangle_pattern.cpp
@@ -4,76 +4,25 @@ using namespace std;
 
 int main(){
 
-    int row,col;
-    cin>>row>>col;
+int row,col;
+cin>>row>>col;
 
-    for (int i=1;i<=row;i++){
-        for(int j=1;j<=col;j++){
-            if (i==1 || i==row){
-                cout<<"*";
-            }
-            else if (j==1 || j==col){
-                cout <<"*";
-            }
-            else{
-                cout <<" ";
-            }
+for (int i=1; i<=row;i++){
+    for (int j=1;j<=col;j++){
+        if (i==1 || i==row || j==1 || j==col){
+            cout<<"*";
+
         }
-        cout <<endl;
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// int row,col;
-// cin>>row>>col;
-
-// for (int i=1; i<=row;i++){
-//     for (int j=1;j<=col;j++){
-//         if (i==1 || i==row || j==1 || j==col){
-//             cout<<"*";
-
-//         }
     //    else if (j==1 || j==col){
     //         cout <<"*";
     //     }
-//         else {
-//             cout<<" ";
-//         }
+        else {
+            cout<<" ";
+        }
         
-//     }
-//     cout<<endl;
-// }
+    }
+    cout<<endl;
+}
 return 0;
 
 }
