1cc25db6c346d9f40d25143e87d6a9226bf8f021
ravi kumar <rk76912@gmail.com>
2021-07-18 15:59:11 +0530

    practice

--- halfpyramid_180rotation.cpp
+++ halfpyramid_180rotation.cpp
@@ -4,58 +4,22 @@ using namespace std;
 int main()
 {
     int n;
-    cin>>n;
-
-    for (int i=n; i>=1;i--){
-        for (int j=1;j<=i;j++){
-            cout<<"*";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (j <= n - i)
+            {
+                cout << "  ";
+            }
+            else
+            {
+                cout << "* ";
+            }
         }
-        cout<<endl;
-
+        cout << endl;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // int n;
-    // cin >> n;
-    // for (int i = 1; i <= n; i++)
-    // {
-    //     for (int j = 1; j <= n; j++)
-    //     {
-    //         if (j <= n - i)
-    //         {
-    //             cout << "  ";
-    //         }
-    //         else
-    //         {
-    //             cout << "* ";
-    //         }
-    //     }
-    //     cout << endl;
-    // }
 
     return 0;
 }
