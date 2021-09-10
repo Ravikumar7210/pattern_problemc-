740732ee8ba963cf2c9b7d80b9352e6c3254e454
ravi kumar <rk76912@gmail.com>
2021-09-10 22:41:37 +0530

    star pattern added

--- advance_pattern_problem/star_pattern.cpp
+++ advance_pattern_problem/star_pattern.cpp
@@ -12,11 +12,8 @@ int main(){
     for ( j=1;j<=n-i;j++){
       cout<<"  ";
     }
-    for (;j<=n;j++){
+    for (j=1;j<=2*i-1;j++){
       cout<<"* ";
-    }
-    for(;j<=n+i-1;j++){
-      cout<<"*"<<" ";
     }
     cout<<endl;
   }
@@ -25,14 +22,39 @@ int main(){
     for ( j=1;j<=n-i;j++){
       cout<<"  ";
     }
-    for (;j<=n;j++){
+    for (j=1;j<=2*i-1;j++){
       cout<<"* ";
-    }
-    for(;j<=n+i-1;j++){
-      cout<<"*"<<" ";
     }
     cout<<endl;
   }
+
+// 2nd method 
+  // for (int i=1;i<=n;i++){
+  //   int j;
+  //   for ( j=1;j<=n-i;j++){
+  //     cout<<"  ";
+  //   }
+  //   for (;j<=n;j++){
+  //     cout<<"* ";
+  //   }
+  //   for(;j<=n+i-1;j++){
+  //     cout<<"*"<<" ";
+  //   }
+  //   cout<<endl;
+  // }
+  // for (int i=n;i>=1;i--){
+  //   int j;
+  //   for ( j=1;j<=n-i;j++){
+  //     cout<<"  ";
+  //   }
+  //   for (;j<=n;j++){
+  //     cout<<"* ";
+  //   }
+  //   for(;j<=n+i-1;j++){
+  //     cout<<"*"<<" ";
+  //   }
+  //   cout<<endl;
+  // }
 
 
   return 0;
